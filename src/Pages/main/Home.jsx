dcf68cc15567c3747525d00571f7c5413077ec8a
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2023-07-04 08:12:37 +0100

    Setup

---
+++ src/Pages/main/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Mainlayout from "../../Layouts/Mainlayout";
+
+function Home() {
+  return (
+    <Mainlayout>
+      <div>Home</div>
+    </Mainlayout>
+  );
+}
+
+export default Home;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className=" text-red-600">
+      <div>NavBar</div>
+    </div>
+  );
+}
+
+export default NavBar;
